37361d06dbbeca9b4a5d3bc396a881336bee86d5
Kyle Haywood <kylehaywood@hotmail.com>
2024-01-16 17:08:55 -0500

    first commit

---
+++ discrminantFormula.py
@@ -0,0 +1,13 @@
+
+# myEquation: 3x^2 + 4x + 5 = 0.
+# discriminantFormula: b^2 – 4ac
+
+a = 0
+b = 0
+c = 0
+
+def discriminant(a,b,c):
+    d = b**2 - 4*a*c
+    print("The discriminant is:", d)
+
+discriminant(3,4,5)
